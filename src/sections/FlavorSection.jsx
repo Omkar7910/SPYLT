cef4a97c3ff4c67ee49c0121abc50dd924378c0a
Omkar7910 <omkarpatil7910@gmail.com>
2025-08-22 22:33:41 +0530

    slider_flavor

--- src/sections/FlavorSection.jsx
+++ src/sections/FlavorSection.jsx
@@ -1,5 +1,5 @@
-import FlavorSlider from "../components/FlavorSlider";
 import FlavorTitle from "../components/FlavorTitle";
+import FlavorSlider from "../components/FlavorSlider";
 
 const FlavorSection = () => {
   return (
@@ -8,7 +8,7 @@ const FlavorSection = () => {
         <div className="lg:w-[57%] flex-none h-80 lg:h-full md:mt-20 xl:mt-0">
           <FlavorTitle />
         </div>
-        <div className="h-full ">
+        <div className="h-full">
           <FlavorSlider />
         </div>
       </div>
